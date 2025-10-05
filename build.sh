910527a5d91b67b90460ee3dab39284c26a497d1
Antheas Kapenekakis <git@antheas.dev>
2025-10-06 01:52:34 +0200

    cleanup and switch to rpmbuild to simplify

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# Key preparation
+#
+
 # Use two keys. In the action, these will get prefilled with the public
 # keys from this directory
 
@@ -31,3 +35,29 @@ done
 # List keys
 echo "Secure Boot Key status:"
 certutil -L -d sql:./certs/pki/ubluesign
+
+#
+# Build
+#
+
+ARCH=${ARCH:-x86_64}
+FEDORA_VERSION=${FEDORA_VERSION:-43}
+
+echo "Starting build for Fedora $FEDORA_VERSION, arch $ARCH"
+
+sudo rm -rf /etc/pki/pesign
+sudo mkdir -p /etc/pki/pesign
+sudo cp -r certs/pki/ubluesign /etc/pki/pesign
+
+rpmbuild \
+  --define '_topdir   %(pwd)/build' \
+  --define '_builddir %{_topdir}/BUILD' \
+  --define '_rpmdir   %{_topdir}/RPMS' \
+  --define '_srcrpmdir %{_topdir}/SRPMS' \
+  --define "_sourcedir $(pwd)/" \
+  --define "_specdir  $(pwd)/" \
+  --with ubsb --with nvidia --with zfs \
+  -ba kernel.spec &
+
+trap 'pkill --signal=SIGKILL -P $$; exit 130' INT
+wait
